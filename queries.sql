ea4e62a9e242643c0d53fab20375ab1b256a7823
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-04-11 15:41:03 -0500

    added last sql statement to query skills from jobs in list, in alphabetical order

--- queries.sql
+++ queries.sql
@@ -16,3 +16,8 @@ DROP TABLE job_skills;
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name ASC;
