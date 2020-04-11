011cd40782243c2f282988960980c28133ea615f
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-04-11 09:41:15 -0500

    set up one to many relationship with employer and job. employer drop down populates, but can't submit form. thinking skill is getting in the way. will move on to step 4 to see.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -16,6 +16,10 @@ public class Employer extends AbstractEntity {
     @Size(min = 1, max = 100, message = "Location must be between 1 and 100 characters!")
     private String location;
 
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
+
     public Employer(){}
 
     public String getLocation() {
@@ -24,5 +28,9 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
